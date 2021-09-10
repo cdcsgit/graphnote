081fe853587bbd65b0f2c131d95f33532affd117
hyungjun <cdcsman@gmail.com>
2021-09-10 18:21:45 +0900

    add data format to help dialog

--- src/com/blogspot/kotlinstudy/graphnote/HelpDialog.kt
+++ src/com/blogspot/kotlinstudy/graphnote/HelpDialog.kt
@@ -53,7 +53,17 @@ private class HelpText() {
         <body>
         <center><font size=7>graph view utility</font><br>
         <font size=5>==================================================================================</font></center>
-
+        <b>Data format</b><br>
+         TITLE|title string <br>
+         SETTINGS|Y Name|X Name|X Range|Y Min value|Min annotation <br>
+         Xval|Line name#Yval#Description1(Can be omitted)|Line name2#Yval2#Description2 ...
+<pre>
+Example
+TITLE|Show CPU Usage : top -b -d 1
+SETTINGS|CPU|TIME|160|0.1|0.5
+1631165418|Total#7.3|999982-top#2.3|6558-Xorg#0.8|6889-cinnamon#0.8|8784-terminator#0.8
+1631165419|Total#0.9|688631-java#0.4|999230-chrome#0.2|6889-cinnamon#0.1
+</pre>
         </body>
         </html>
     """.trimIndent()
